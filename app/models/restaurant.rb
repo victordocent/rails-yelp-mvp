6abe96bb2d77ee81891e5370fb171e49e734423d
Victor Bostaetter <victor.bostaetter@docent-art.com>
2023-11-15 11:15:57 +0100

    validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, :adress, :category, presence: true
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
 
   has_many :reviews, dependent: :destroy
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: 0..5 }
   belongs_to :restaurant
 end
