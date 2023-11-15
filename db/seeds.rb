1280a114a0d7270b76e084b7acba3f99869ccd01
Victor Bostaetter <victor.bostaetter@docent-art.com>
2023-11-15 12:20:58 +0100

    routes configured

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,36 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom",
+          address: "7 Boundary St, London E2 7JE",
+          category: "japanese",
+          phone_number: "+44 3456789O567"
+        }
+pizza_east =  {name: "Pizza East",
+              address: "56A Shoreditch East St, London E1 6PQ",
+              category: "italian",
+              phone_number: "+44 3456789O567"
+            }
+pizza_west =  {name: "Pizza West",
+              address: "56A Shoreditch West St, London E1 7PQ",
+              category: "italian",
+              phone_number: "+44 3456789O567"
+            }
+pizza_south =  {name: "Pizza South",
+              address: "56A Shoreditch South St, London E1 8PQ",
+              category: "italian",
+              phone_number: "+44 3456789O567"
+            }
+pizza_north=  {name: "Pizza North",
+              address: "56A Shoreditch North St, London E1 9PQ",
+              category: "italian",
+              phone_number: "+44 3456789O567"
+            }
+
+
+[dishoom, pizza_east, pizza_west, pizza_south, pizza_north].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
