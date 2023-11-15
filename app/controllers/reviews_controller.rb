c50e0ba4b5c7a6c88653665c269517396eacc777
Victor Bostaetter <victor.bostaetter@docent-art.com>
2023-11-15 12:52:03 +0100

    functionnal app

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  # We need to find the restaurant associated with the review
+  before_action :set_restaurant, only: %i[new create]
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def new
+    @review = Review.new
+  end
+
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :restaurants, only: [:index, :show, :new, :create] do
